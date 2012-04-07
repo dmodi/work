d57eb11d1e00caa46a4c4ddcc6f56cc9ecd90528
Dharmendra Modi <dharmendra.modi.86@gmail.com>
2012-04-08 00:32:35 +0530

    Modified palindrom.c.
    * Added proper indentation.
    * Minor code cleanup.
    
    Signed-off-by: Dharmendra Modi <dharmendra.modi.86@gmail.com>

--- palindrom.c
+++ palindrom.c
@@ -1,22 +1,31 @@
-	# include<stdio.h>
-	int main()
+# include<stdio.h>
+int main()
+{
+
+	int prevRem = 0;
+	int num; 
+	int rem;
+	int newNum;
+
+	printf ("Enter the number :");
+	scanf ("%d", &num);
+
+	newNum = num;
+
+	while (newNum != 0)
 	{
-		
-		int prevRem = 0, num , rem ;
-		int newNum = num;
-		while(newNum !=0)
-		{
-			rem = newNum % 10 ;
-			newNum = newNum / 10 ;
-			prevRem = prevRem * 10 + rem ;
-		}
-			if(prevRem == num)
-			{
-			printf("palindrom\n");
-			}
-			else
-			{
-			printf("not a palindrom\n");
-			}
+		rem = newNum % 10 ;
+		newNum = newNum / 10 ;
+		prevRem = prevRem * 10 + rem ;
 	}
-		
+	if (prevRem == num)
+	{
+		printf("%d is palindrom\n", num);
+	}
+	else
+	{
+		printf("%d is not a palindrom\n", num);
+	}
+	return 0;
+}
+
